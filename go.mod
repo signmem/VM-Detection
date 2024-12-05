790402faf36cc30c19fc928f0409ab339e3bff84
terry.zeng <terry.zeng@vipshop.com>
2024-12-05 16:56:38 +0800

    fork by github.com/ShellCode33/VM-Detection

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/signmem/VM-Detection
+
+go 1.18
+
+require (
+	github.com/klauspost/cpuid v1.3.1
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	golang.org/x/sys v0.28.0
+)
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+        "fmt"
+	"github.com/signmem/VM-Detection/vmdetect"
+)
+
+func main() {
+        fmt.Println("Trying to detect if a VM is running...")
+
+        isInsideVM, reason := vmdetect.IsRunningInVirtualMachine()
+
+        if isInsideVM {
+                fmt.Printf("\nVM detected thanks to %v\n", reason)
+        } else {
+                fmt.Println("\nNo VM detected")
+        }
+
+}
